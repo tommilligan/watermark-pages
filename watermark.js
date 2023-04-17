792afc7d74e4281e5cb8ff43ff59fbf251301a40
Tom Milligan <tom.milligan@uipath.com>
2023-04-17 19:50:42 +0100

    working from data object

--- watermark.js
+++ watermark.js
@@ -1,4 +1,18 @@
-var div = document.createElement("div");
-document.body.appendChild(div);
-div.id = "watermark-production";
-div.innerText = "PRODUCTION";
+function createWatermark(text, color) {
+  var div = document.createElement("div");
+  document.body.appendChild(div);
+  div.id = "watermark";
+  div.innerText = text;
+  div.style = "color:" + color;
+}
+
+chrome.runtime.sendMessage(
+  undefined,
+  window.location.href,
+  undefined,
+  (response) => {
+    if (response) {
+      createWatermark(response.text, response.color);
+    }
+  }
+);
