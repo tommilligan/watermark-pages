b23a8f9a7ec37da8336d20d8e3583af1cac5e7e2
Tom Milligan <tom.milligan@uipath.com>
2023-04-18 18:17:00 +0100

    rm serviceworker

--- watermark.js
+++ watermark.js
@@ -6,13 +6,49 @@ function createWatermark(text, color) {
   div.style = "color:" + color;
 }
 
-chrome.runtime.sendMessage(
-  undefined,
-  window.location.href,
-  undefined,
-  (response) => {
-    if (response) {
-      createWatermark(response.text, response.color);
-    }
+async function getData() {
+  let data = await chrome.storage.local.get(["data"]);
+  console.log("Got data", data);
+  data = {
+    rules: [
+      {
+        path: "https://cloud.uipath.com/.*",
+        data: {
+          text: "PRODUCTION",
+          color: "red",
+        },
+      },
+      {
+        path: "https://developer.chrome.com/.*",
+        data: {
+          text: "TEST",
+          color: "orange",
+        },
+      },
+    ],
+  };
+
+  return {
+    rules: data.rules.map(({ path, data }) => {
+      return { path: new RegExp(path), data };
+    }),
+  };
+}
+
+async function getPathData(href) {
+  const data = await getData();
+  var result = data.rules.find((rule) => href.match(rule.path));
+  if (result) {
+    return result.data;
   }
-);
+}
+
+async function main() {
+  const href = window.location.href;
+  const data = await getPathData(href);
+  if (data) {
+    createWatermark(data.text, data.color);
+  }
+}
+
+main().catch(console.error);
